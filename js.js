e68fad7323452561ff5bc6514db936ba8d27d812
Bar1126 <barsho26@gmail.com>
2025-05-21 11:18:23 +0300

    Add files via upload
    browser events

---
+++ js.js
@@ -0,0 +1,22 @@
+'use strict';
+
+//browser events
+
+//task1 +2
+// let h1 = document.getElementById('mainTitle');
+// let changeColor = () => (h1.style.color = 'blue')
+// let hide = () => (h1.style.display = 'none')
+// let show = () => (h1.style.display = 'block')
+
+//task3
+// let h1 = document.getElementById('mainTitle')
+// let changeText = () => (h1.textContent = 'text changed!')
+
+//task4
+// let p1 = document.getElementById('p1')
+// let hideShow = () => (p1.style.display = p1.style.display ==='none' ? 'block' : 'none')
+
+//task5
+const hoverBox = document.getElementById('colorBox');
+let changeColor = () => (hoverBox.style.backgroundColor = 'purple');
+let resetColor = () => (hoverBox.style.backgroundColor = 'lightblue');
